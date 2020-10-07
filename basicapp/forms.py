75e0ba2d9978616f2f14e052336bfaeee1f1991c
MoutiaFazaa <fazaamouty@gmail.com>
2020-10-07 12:07:53 -0400

    Validators and Creating them

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,6 +1,25 @@
 from django import forms
+from django.core import validators
+
+#Writing our own validators
+def check_for_z(value):
+    #check for Z letter and 0 value
+    if value[0].lower() != 'z':
+        raise forms.ValidationError('NAME NEEDS TO Start WITH Z')
 
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+
+
+    #Protrction from bots
+    botcatcher = forms.CharField(required=False,
+                                widget=forms.HiddenInput,
+                                validators=[validators.MaxLengthValidator(0)])
+
+    #def clean_botcatcher (self):
+     #   botcatcher = self.cleaned_data['botcatcher']
+      #  if len(botcatcher)> 0:
+       #     raise forms.ValidationError('Gotcha Bot!')
+        #return botcatcher
